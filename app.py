60e5707d5d59ea156b705aa96e68197e3ae63b1e
Mauricio <102171726+maurodevis@users.noreply.github.com>
2023-08-29 18:28:15 -0500

    archivos actualizados y funcionales

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from .modelos import db, Cancion
+from .modelos import db, Usuario, Album, Medio
 
 app = create_app('default')
 app_context = app.app_context()
@@ -9,8 +9,14 @@ db.init_app(app)
 db.create_all()
 
 with app.app_context():
-    c = Cancion(titulo='Prueba', minutos=2, segundos=25, interprete='-juan Pablo')
-    db.session.add(c)
+    u = Usuario(nombre='Alejo', contrasena='Alejo254')
+    a = Album(titulo="Aventuras maquiavelicas", anio='1980', descripcion='Lo mejor de los mejor', medio=Medio.CD)
+    u.albumes.append(a)
+    db.session.add(u)
     db.session.commit()
-    print(Cancion.query.all())
+    print(Usuario.query.all())
+    print(Usuario.query.all()[0].albumes)
+    db.session.delete(u)
+    print(Usuario.query.all())
+    print(Album.query.all())
 
